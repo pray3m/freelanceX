9d40ed34b4b7ccc79cdbe9ba32c52ee959ad2b6f
Prem Gautam <prem.gtm9@gmail.com>
2023-05-17 23:09:05 +0545

    setup : express server mongodb conn

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import connectDB from "./config/database.js";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 app.use(
   cors({
