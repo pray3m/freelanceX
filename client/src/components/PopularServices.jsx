c2ee55f13dd02c2df009c67806dba14a22a2cd07
pray3m <prem.gtm9@gmail.com>
2023-05-20 02:04:09 +0545

    feat (PopularServices) : add popularservices comp

--- client/src/components/PopularServices.jsx
+++ client/src/components/PopularServices.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import Image from "next/image";
+import { useRouter } from "next/router";
+import React from "react";
 
 const PopularServices = () => {
-  return (
-    <div>PopularServices</div>
-  )
-}
+  const router = useRouter();
 
-export default PopularServices
+  const popularServices = [
+    { name: "Ai Artists", label: "Add talent to AI", image: "/service1.png" },
+    { name: "Logo Design", label: "Build your brand", image: "/service2.jpeg" },
+    {
+      name: "Wordpress",
+      label: "Customize your site",
+      image: "/service3.jpeg",
+    },
+    {
+      name: "Voice Over",
+      label: "Share your message",
+      image: "/service4.jpeg",
+    },
+    {
+      name: "Social Media",
+      label: "Reach more customers",
+      image: "/service5.jpeg",
+    },
+    { name: "SEO", label: "Unlock growth online", image: "/service6.jpeg" },
+    {
+      name: "Illustration",
+      label: "Color your dreams",
+      image: "/service7.jpeg",
+    },
+    { name: "Translation", label: "Go global", image: "/service8.jpeg" },
+  ];
+
+  return (
+    <div className="mx-6 md:mx-20 my-6 md:my-12">
+      <h2 className="text-2xl md:text-4xl mb-3 md:mb-5 text-[#404145] font-bold">
+        Popular Services
+      </h2>
+      <ul className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+        {popularServices.map(({ name, label, image }) => (
+          <li
+            key={name}
+            className="relative cursor-pointer "
+            onClick={() => router.push(`/search?q=${name.toLowerCase()}`)}
+          >
+            <div className="absolute z-10 text-white left-3 top-3">
+              <span className="text-sm md:text-base"> {label}</span>
+              <h6 className="font-semibold text-lg md:text-2xl ">{name}</h6>
+            </div>
+            <div className="h-48 md:h-80 w-full relative">
+              <div className="absolute inset-0 object-contain	">
+                <Image src={image} alt="service" fill />
+              </div>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PopularServices;
