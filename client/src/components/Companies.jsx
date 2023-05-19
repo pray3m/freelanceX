5c107b8b5655ebd47845f0bc5e2d212f01e86586
pray3m <prem.gtm9@gmail.com>
2023-05-20 00:21:33 +0545

    feat (companies) : add companies component

--- client/src/components/Companies.jsx
+++ client/src/components/Companies.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
-const Companies = () => {
+function Companies() {
+  const trustedCompanies = [1, 2, 3, 4, 5];
   return (
-    <div>Companies</div>
-  )
+    <div className="flex items-center justify-center text-gray-400 text-2xl font-semibold min-h-[11vh] mx-2 ">
+      <p className="hidden md:inline text-xl">Trusted By :</p>
+      <ul className="flex max-w-full justify-between gap-10 mx-6">
+        {trustedCompanies.map((num) => (
+          <li key={num} className="relative h-[4rem] w-[4rem]">
+            <Image src={`/trusted${num}.png`} alt="trusted brands" fill />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default Companies
+export default Companies;
