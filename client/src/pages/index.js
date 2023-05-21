e35738d6fd30951edf5fc69faad64cacdbdb9626
pray3m <prem.gtm9@gmail.com>
2023-05-21 23:13:42 +0545

    refactor: rearrange Landing page components

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,10 +1,10 @@
-import Business from "@/components/Business";
-import Companies from "@/components/Companies";
-import Everything from "@/components/Everything";
-import HeroBanner from "@/components/HeroBanner";
-import JoinFreelance from "@/components/JoinFreelance";
-import PopularServices from "@/components/PopularServices";
-import Services from "@/components/Services";
+import Business from "@/components/Landing/Business";
+import Companies from "@/components/Landing/Companies";
+import Everything from "@/components/Landing/Everything";
+import HeroBanner from "@/components/Landing/HeroBanner";
+import JoinFreelance from "@/components/Landing/JoinFreelance";
+import PopularServices from "@/components/Landing/PopularServices";
+import Services from "@/components/Landing/Services";
 import React from "react";
 
 const Index = () => {
