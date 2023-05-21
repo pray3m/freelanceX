b0bd296d989d1f562a0e94926702ba8b841c4863
pray3m <prem.gtm9@gmail.com>
2023-05-21 20:05:18 +0545

    feat(JoinX) : create join component

--- client/src/components/JoinFreelance.jsx
+++ client/src/components/JoinFreelance.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
-const JoinFreelance = () => {
+function JoinFiverr() {
   return (
-    <div>JoinFreelance</div>
-  )
+    <div className=" mx-4 my-6 md:mx-32 md:my-16 relative">
+      <div className="absolute z-10 top-1/3 left-5 md:left-10">
+        <h4 className="text-white text-4xl md:text-5xl mb-10 font-semibold">
+          Suddenly it's all so <i>doable.</i>
+        </h4>
+        <button
+          className="border text-base font-medium px-5 py-2 md:mx-8 border-[#1DBF73] bg-[#1DBF73] text-white rounded-md"
+          type="button"
+        >
+          Join FreelanceX
+        </button>
+      </div>
+      <div className="brightness-90 md:brightness-100 h-80 w-full md:h-96">
+        <Image src="/bg-signup.webp" fill alt="signup" className="rounded-sm" />
+      </div>
+    </div>
+  );
 }
 
-export default JoinFreelance
+export default JoinFiverr;
