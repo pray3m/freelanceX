787ff55fbabf5e148a68dc7798e8fddef0c85f92
pray3m <prem.gtm9@gmail.com>
2023-05-22 22:44:19 +0545

    feat(modal) : create login/signup component

---
+++ client/src/components/AuthWrapper.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { FcGoogle } from "react-icons/fc";
+import { MdFacebook } from "react-icons/md";
+
+const AuthWrapper = () => {
+  return (
+    <div className="fixed top-0 z-[100]">
+      <div className="h-[100vh] w-[100vw] backdrop-blur-md fixed top "></div>
+      <div className="h-[100vh] w-[100vw] flex flex-col justify-center items-center">
+        <div
+          className="fixed z-[101] h-max w-max bg-white rounded-lg shadow-lg flex flex-col justify-center items-center"
+          id="auth-modal"
+        >
+          <div className="flex flex-col justify-center items-center p-8 gap-7">
+            <h3 className="text-2xl font-semibold text-slate-700">
+              Join freelanceX
+            </h3>
+            <div className="flex flex-col gap-5">
+              <button className="text-white bg-blue-500 p-3 font-semibold w-80 flex items-center justify-center relative">
+                <MdFacebook className="absolute left-4 text-2xl" />
+                Continue with Facebook
+              </button>
+              <button className="border border-slate-300 p-3 font-medium w-80 flex items-center justify-center relative">
+                <FcGoogle className="absolute left-4 text-2xl" />
+                Continue with Google
+              </button>
+            </div>
+            <div className="relative  w-full text-center">
+              <span className="before:content-[''] before:h-[0.5px] before:w-80 before:absolute before:top-[50%] before:left-0 before:bg-slate-400">
+                <span className="bg-white relative z-10 px-2">OR</span>
+              </span>
+            </div>
+
+            <div className="flex flex-col gap-5">
+              <input
+                type="text"
+                name="email"
+                placeholder="Email / Username"
+                className="border border-slate-300 p-3 w-80"
+                // onChange={handleChange}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                className="border border-slate-300 p-3 w-80"
+                name="password"
+                // onChange={handleChange}
+              />
+              <button
+                className="bg-[#1DBF73] text-white px-12 text-lg font-semibold rounded-r-md p-3 w-80"
+                // onClick={handleClick}
+                type="button"
+              >
+                Continue
+              </button>
+            </div>
+          </div>
+          <div className="py-5 w-full flex items-center justify-center border-t border-slate-400">
+            <span className="text-sm  text-slate-700">
+              Not a member yet?
+              <span className="text-[#1dbf73] cursor-pointer"> Join Now </span>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AuthWrapper;
