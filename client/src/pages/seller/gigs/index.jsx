a9e31918d3591dd2adad2d9e236be55e624bc984
pray3m <prem.gtm9@gmail.com>
2023-06-05 11:08:14 +0545

    feat(gigs) : add all gigs table

--- client/src/pages/seller/gigs/index.jsx
+++ client/src/pages/seller/gigs/index.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { GET_ALL_USER_GIGS_ROUTE } from "../../../utils/constants";
 import axios from "axios";
+import Link from "next/link";
 
 const index = () => {
   const [gigs, setGigs] = useState([]);
@@ -20,7 +21,59 @@ const index = () => {
     getUserGigs();
   }, []);
 
-  return <div>index</div>;
+  return (
+    <div className="min-h-[80vh] my-10 mt-0 px-32 ">
+      <h3 className="m-5 text-3xl font-semibold"> All Your Gigs</h3>
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="px-6 py-3">
+                Title
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Category
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Delivery Time
+              </th>
+              <th scope="col" className="px-6 py-3">
+                Price
+              </th>
+              <th scope="col" className="px-6 py-3">
+                <span className="sr-only">Edit</span>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {gigs.map(({ title, category, deliveryTime, price, id }) => {
+              return (
+                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                  <th
+                    scope="row"
+                    className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                  >
+                    {title}
+                  </th>
+                  <td className="px-6 py-4">{category}</td>
+                  <td className="px-6 py-4">{deliveryTime} Days</td>
+                  <td className="px-6 py-4">$ {price}</td>
+                  <td className="px-6 py-4 text-right">
+                    <Link
+                      className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                      href={`/seller/gigs/${id}`}
+                    >
+                      Edit
+                    </Link>
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default index;
