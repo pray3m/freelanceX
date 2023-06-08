5485207c24fcfd9db8357a655252eb39f4834c16
pray3m <prem.gtm9@gmail.com>
2023-06-08 23:38:38 +0545

    feat(search) : add search result component

--- client/src/pages/search.jsx
+++ client/src/pages/search.jsx
@@ -2,11 +2,12 @@ import { SEARCH_GIGS_ROUTE } from "../utils/constants";
 import axios from "axios";
 import { useRouter } from "next/router";
 import React, { useEffect, useState } from "react";
+import SearchGridItem from "../components/search/SearchGridItem";
 
 const search = () => {
   const router = useRouter();
   const { category, q } = router.query;
-  const [gigs, setGigs] = useState(undefined);
+  const [gigs, setGigs] = useState([]);
 
   useEffect(() => {
     const getData = async () => {
@@ -24,7 +25,38 @@ const search = () => {
     if (category || q) getData();
   }, [category, q]);
 
-  return <div>search</div>;
+  return (
+    <div className="mx-24 mb-24">
+      {q && (
+        <h3 className="text-4xl mb-10">
+          Results for <b>{q}</b>
+        </h3>
+      )}
+      <div className="flex gap-4">
+        <button className="py-3 px-5 border border-gray-400 rounded-lg font-medium">
+          Category
+        </button>
+        <button className="py-3 px-5 border border-gray-400 rounded-lg font-medium">
+          Budget
+        </button>
+        <button className="py-3 px-5 border border-gray-400 rounded-lg font-medium">
+          Delivery Time
+        </button>
+      </div>
+      <div>
+        <div className="my-4">
+          <span className="text-[#74767e] font-medium ">
+            {gigs.length} services available
+          </span>
+        </div>
+        <div className="grid grid-cols-4">
+          {gigs.map((gig) => (
+            <SearchGridItem gig={gig} key={gig.id} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default search;
--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,5 +1,6 @@
 export const HOST = process.env.NEXT_PUBLIC_SERVER_URL;
 export const API_URL = `${HOST}/api`;
+export const IMAGES_URL = `${HOST}/uploads`;
 
 export const AUTH_ROUTES = `${API_URL}/auth`;
 export const GIG_ROUTES = `${API_URL}/gigs`;
