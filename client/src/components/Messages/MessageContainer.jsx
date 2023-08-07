ea7383879b1889a5f801dbef86b3e351c3647bce
pray3m <prem.gtm9@gmail.com>
2023-08-07 12:53:57 +0545

    feat: implement send messages

--- client/src/components/Messages/MessageContainer.jsx
+++ client/src/components/Messages/MessageContainer.jsx
@@ -1,9 +1,10 @@
 import { useRouter } from "next/router";
 import axios from "axios";
 import { useCookies } from "react-cookie";
+import { FaRegPaperPlane } from "react-icons/fa";
 import { useStateProvider } from "../../context/StateContext";
 import { useEffect, useState } from "react";
-import { GET_MESSAGES } from "../../utils/constants";
+import { GET_MESSAGES, SEND_MESSAGE } from "../../utils/constants";
 
 const MessageContainer = () => {
   const [cookies] = useCookies();
@@ -23,7 +24,7 @@ const MessageContainer = () => {
           },
         });
         setMessages(data.messages);
-        setReceiverId(data.receiver);
+        setReceiverId(data.receiverId);
       } catch (err) {
         console.log(err);
       }
@@ -34,9 +35,97 @@ const MessageContainer = () => {
     }
   }, [orderId, userInfo]);
 
-  console.log(messages);
+  function formatTime(timestamp) {
+    const date = new Date(timestamp);
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    const ampm = hours >= 12 ? "PM" : "AM";
+    hours %= 12;
+    hours = hours || 12;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    const formattedTime = `${hours}:${minutes} ${ampm}`;
+    return formattedTime;
+  }
 
-  return <div>MessageContainer</div>;
+  const sendMessage = async () => {
+    if (messageText.length) {
+      const response = await axios.post(
+        `${SEND_MESSAGE}/${orderId}`,
+        {
+          message: messageText,
+          receiverId,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${cookies.jwt}`,
+          },
+        }
+      );
+      if (response.status === 201) {
+        setMessages([...messages, response.data.message]);
+        setMessageText("");
+      }
+    }
+  };
+
+  return (
+    <div className="h-[80vh]">
+      <div className="max-h-[80vh]   flex flex-col justify-center items-center">
+        <div className="bg-white py-8 px-4 shadow-2xl sm:rounded-lg sm:px-10 w-[80vw] border flex flex-col">
+          <div className="mt-8">
+            <div className="space-y-4 h-[50vh] overflow-y-auto pr-4 ">
+              {messages.map((message) => (
+                <div
+                  key={message.id}
+                  className={`flex ${
+                    message.senderId === userInfo.id
+                      ? "justify-end"
+                      : "justify-start"
+                  }`}
+                >
+                  <div
+                    className={`inline-block rounded-lg ${
+                      message.senderId === userInfo.id
+                        ? "bg-[#1DBF73] text-white"
+                        : "bg-gray-100 text-gray-800"
+                    } px-4 py-2 max-w-xs break-all`}
+                  >
+                    <p>{message.text}</p>
+                    <span className="text-sm text-gray-600">
+                      {formatTime(message.createdAt)}
+                    </span>
+                    <span>
+                      {message.senderId === userInfo.id && message.isRead && (
+                        <BsCheckAll />
+                      )}
+                    </span>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+
+          <div className="mt-8 flex">
+            <input
+              type="text"
+              className="rounded-full py-2 px-4 mr-2 w-full"
+              placeholder="Type a message..."
+              name="message"
+              onChange={(e) => setMessageText(e.target.value)}
+              value={messageText}
+            />
+            <button
+              type="submit"
+              className="bg-[#1DBF73] text-white rounded-full px-4 py-2"
+              onClick={sendMessage}
+            >
+              <FaRegPaperPlane />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MessageContainer;
