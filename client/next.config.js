dfb8cca750f470f5b82bf1215167fbff31534ebe
pray3m <prem.gtm9@gmail.com>
2023-08-08 12:36:23 +0545

    fixed the deploy error

--- client/next.config.js
+++ client/next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: process.env.NODE_ENV !== "production",
   images: {
     remotePatterns: [
       {
         protocol: process.env.NODE_ENV === "production" ? "https" : "http",
-        hostname: process.env.NODE_ENV === "production" ? "**" : "localhost",
+        hostname:
+          process.env.NODE_ENV === "production"
+            ? "freelancex.onrender.com"
+            : "localhost",
         port: process.env.NODE_ENV === "production" ? "" : "5000",
         pathname: "**",
       },
